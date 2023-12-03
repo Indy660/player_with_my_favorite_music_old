322b97d57f5243d8b549e433fadbd7c1d2f9ee21
Indy <indycar111@gmail.com>
2023-12-03 22:55:54 +0300

    add logic base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/player_with_my_favorite_music/"
+      : "/",
+});
